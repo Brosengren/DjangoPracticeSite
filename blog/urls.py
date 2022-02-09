127626f55f500138728e35b5efaeab1f5d8c38f0
Braden <bradenrosengren@gmail.com>
2022-02-09 10:32:41 -0600

    initial commit

---
+++ blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path("", views.starting_page, name="starting-page"),
+    path("posts", views.posts, name="posts-page"),
+    path("posts/<slug:slug>", views.post_detail, name="post-detail-page"), 
+    #blog/my-first-post slug:slug makes sure only urls with a correct path will trigger
+]
---
+++ mysite/urls.py
@@ -0,0 +1,32 @@
+"""mysite URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/3.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path, include
+from django.views.generic.base import TemplateView
+from . import views
+
+urlpatterns = [
+    path('base/', TemplateView.as_view(template_name="base.html")),#TODO:make this '' for home page
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')), #TODO:set this to blog/ later to add projects, sites, recpies, etc.
+    path('demo404', TemplateView.as_view(template_name="404.html")),
+    path('demo500', TemplateView.as_view(template_name="500.html"))
+]
+
+handler400 = 'mysite.views.error_400'
+handler403 = 'mysite.views.error_403'
+handler404 = 'mysite.views.error_404'
+handler500 = 'mysite.views.error_500'
